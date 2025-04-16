dab194ef63c932120adfdb2b86a0b1c0a1d7db6b
q269384828 <269384828@qq.com>
2025-04-16 15:37:04 +0800

    添加部署脚本以支持 gh-pages 发布，并更新 Vite 配置以使用 package.json 中的项目名称

---
+++ deploy.mjs
@@ -0,0 +1,9 @@
+// git@github.com:q269384828/blcok-note-vue-demo.git
+import ghpages from 'gh-pages'
+
+ghpages.publish('dist', {
+  branch: 'gh-pages',
+  repo: 'https://github.com/q269384828/blcok-note-vue-demo.git',
+}, (e) => {
+  console.log('success', e)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import vue from '@vitejs/plugin-vue';
-
-// https://vite.dev/config/
+import pkg from './package.json';
 export default defineConfig({
+  base: pkg.name,
   plugins: [react(), vue()],
 });
